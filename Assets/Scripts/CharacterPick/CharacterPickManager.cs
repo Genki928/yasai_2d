99992cdba8cb7f5207f01bc1bebe1d163eb1c317
DESKTOP-U8RCPBA\GA2B <h1202515016@hiratagakuen.onmicrosoft.com>
2026-05-27 12:00:21 +0900

    test

--- Assets/Scripts/CharacterPick/CharacterPickManager.cs
+++ Assets/Scripts/CharacterPick/CharacterPickManager.cs
@@ -6,21 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class CharacterPickManager : MonoBehaviour
 {
+    [Header("◇アイコン")]
     [SerializeField] GameObject icon_pf;
     [SerializeField] List<Sprite> icon_img = new();
+    List<GameObject> icon_obj = new();
+    Vector2 pos = new(0, 0);
+
     [Header("◇カーソル")]
     [SerializeField] GameObject cursor_pf;
     [SerializeField] Cursor[] cursor = new Cursor[2];
     [SerializeField] Sprite mix_cursor;
-    List<GameObject> icon_obj = new();
     List<GameObject> cursor_obj = new();
+
+    [Header("◇モデル")]
+    [SerializeField] GameObject[] model = new GameObject[2];
 
     const float ICON_HORIZONTAL_SPACE = 1.5f;
     const float ICON_VERTICAL_SPACE = 1.4f;
     const int ICON_LINEFEED_COUNT = 3;
     const int X = 0;
     const int Y = 1;
-    public Vector2 pos = new(0, 0);
 
     void Start()
     {
@@ -43,6 +48,8 @@ public class CharacterPickManager : MonoBehaviour
         }
         cursor_obj.Add(Instantiate(cursor_pf));
         cursor_obj[0].GetComponent<SpriteRenderer>().sprite = cursor[0].img;
+        cursor_obj.Add(Instantiate(cursor_pf));
+        cursor_obj[1].GetComponent<SpriteRenderer>().sprite = cursor[1].img;
         //cursor[0].pos = icon_obj[0].transform.position;
     }
 
@@ -62,16 +69,16 @@ public class CharacterPickManager : MonoBehaviour
     {
         if (ctx.performed)
         {
-            if (--cursor[0].pos[Y] < 0) cursor[0].pos[Y] = icon_obj.Count / ICON_LINEFEED_COUNT;
-            if (cursor[0].pos[Y] == icon_obj.Count / ICON_LINEFEED_COUNT)
-            {
-                if (cursor[0].pos[X] > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT)
-                {
-                    cursor[0].pos[X] = (icon_obj.Count - 1) % ICON_LINEFEED_COUNT;
-                }
-            }
+            //if (--cursor[0].pos[Y] < 0) cursor[0].pos[Y] = icon_obj.Count / ICON_LINEFEED_COUNT;
+            //if (cursor[0].pos[Y] == icon_obj.Count / ICON_LINEFEED_COUNT)
+            //{
+            //    if (cursor[0].pos[X] > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT)
+            //    {
+            //        cursor[0].pos[X] = (icon_obj.Count - 1) % ICON_LINEFEED_COUNT;
+            //    }
+            //}
 
-            cursor_obj[0].transform.position = new(pos.x + ICON_HORIZONTAL_SPACE * cursor[0].pos[X], pos.y - ICON_VERTICAL_SPACE * cursor[0].pos[Y]);
+            //cursor_obj[0].transform.position = new(pos.x + ICON_HORIZONTAL_SPACE * cursor[0].pos[X], pos.y - ICON_VERTICAL_SPACE * cursor[0].pos[Y]);
         }
     }
 
@@ -79,14 +86,14 @@ public class CharacterPickManager : MonoBehaviour
     {
         if (ctx.performed)
         {
-            if (++cursor[0].pos[Y] > (icon_obj.Count - 1) / ICON_LINEFEED_COUNT) cursor[0].pos[Y] = 0;
-            if (icon_obj.Count % ICON_LINEFEED_COUNT != 0)
-            {
-                if (cursor[0].pos[X] > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT) cursor[0].pos[Y] = 0;
-            }
+            //if (++cursor[0].pos[Y] > (icon_obj.Count - 1) / ICON_LINEFEED_COUNT) cursor[0].pos[Y] = 0;
+            //if (icon_obj.Count % ICON_LINEFEED_COUNT != 0)
+            //{
+            //    if (cursor[0].pos[X] > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT) cursor[0].pos[Y] = 0;
+            //}
 
 
-            cursor_obj[0].transform.position = new(pos.x + ICON_HORIZONTAL_SPACE * cursor[0].pos[X], pos.y - ICON_VERTICAL_SPACE * cursor[0].pos[Y]);
+            //cursor_obj[0].transform.position = new(pos.x + ICON_HORIZONTAL_SPACE * cursor[0].pos[X], pos.y - ICON_VERTICAL_SPACE * cursor[0].pos[Y]);
         }
     }
 
@@ -94,14 +101,21 @@ public class CharacterPickManager : MonoBehaviour
     {
         if (ctx.performed)
         {
-            if (--cursor[0].pos[X] < 0) cursor[0].pos[X] = ICON_LINEFEED_COUNT - 1;
+            // 識別
+            int n = -1;
+            if (Gamepad.all[0] == ctx.control.device) n = 0;
+            else n = 1;
+
+            // 移動
+            if (--cursor[n].pos[X] < 0) cursor[n].pos[X] = ICON_LINEFEED_COUNT - 1;
 
             // もしカーソルのX座標がアイコンのある位置から外れていたら、座標を右端に整える
-            if (cursor[0].pos[Y] == icon_obj.Count / ICON_LINEFEED_COUNT)
-                if (cursor[0].pos[X] % ICON_LINEFEED_COUNT > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT)
-                    cursor[0].pos[X] = (icon_obj.Count - 1) % ICON_LINEFEED_COUNT;
+            if (cursor[n].pos[Y] == icon_obj.Count / ICON_LINEFEED_COUNT)
+                if (cursor[n].pos[X] % ICON_LINEFEED_COUNT > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT)
+                    cursor[n].pos[X] = (icon_obj.Count - 1) % ICON_LINEFEED_COUNT;
 
-            cursor_obj[0].transform.position = new(pos.x + ICON_HORIZONTAL_SPACE * cursor[0].pos[X], pos.y - ICON_VERTICAL_SPACE * cursor[0].pos[Y]);
+            // 描画
+            Draw(n);
         }
     }
 
@@ -109,15 +123,30 @@ public class CharacterPickManager : MonoBehaviour
     {
         if (ctx.performed)
         {
-            if (++cursor[0].pos[X] > ICON_LINEFEED_COUNT - 1) cursor[0].pos[X] = 0;
+            // 識別
+            int n = -1;
+            if (Gamepad.all[0] == ctx.control.device) n = 0;
+            else n = 1;
+
+            // 移動
+            if (++cursor[n].pos[X] > ICON_LINEFEED_COUNT - 1) cursor[n].pos[X] = 0;
 
             // もしカーソルのX座標がアイコンのある位置から外れていたら、座標を左端に整える
-            if (cursor[0].pos[Y] == icon_obj.Count / ICON_LINEFEED_COUNT)
-                if (cursor[0].pos[X] % ICON_LINEFEED_COUNT > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT)
-                    cursor[0].pos[X] = 0;
+            if (cursor[n].pos[Y] == icon_obj.Count / ICON_LINEFEED_COUNT)
+                if (cursor[n].pos[X] % ICON_LINEFEED_COUNT > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT)
+                    cursor[n].pos[X] = 0;
 
-            cursor_obj[0].transform.position = new(pos.x + ICON_HORIZONTAL_SPACE * cursor[0].pos[X], pos.y - ICON_VERTICAL_SPACE * cursor[0].pos[Y]);
+            // 描画
+            Draw(n);
         }
+    }
+
+    void Draw(int n)
+    {
+        // 描画
+        Debug.Log(cursor[n].pos[Y] / ICON_LINEFEED_COUNT + cursor[n].pos[X]);
+        cursor_obj[n].transform.position = new(pos.x + ICON_HORIZONTAL_SPACE * cursor[n].pos[X], pos.y - ICON_VERTICAL_SPACE * cursor[n].pos[Y]);
+        model[n].GetComponent<SpriteRenderer>().sprite = icon_img[cursor[n].pos[Y] * ICON_LINEFEED_COUNT + cursor[n].pos[X]];
     }
 }
 
@@ -126,4 +155,10 @@ public class Cursor
 {
     public Sprite img;
     public int[] pos = new int[2] { 0, 0 };
+}
+
+[CreateAssetMenu(menuName = "Character/PickData")]
+public class PickData : ScriptableObject
+{
+    
 }
