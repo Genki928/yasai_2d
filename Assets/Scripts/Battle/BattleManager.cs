5b85b9b17db6388d17213e7e588e45e431c554c5
DESKTOP-A66LBOP\GA2B <h1202515021@hiratagakuen.onmicrosoft.com>
2026-05-27 10:24:42 +0900

    祝　コントローラー識別

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.InputSystem;
 public class BattleManager : MonoBehaviour
 {
     [Header("◇キャラ生成")]
@@ -25,27 +25,79 @@ public class BattleManager : MonoBehaviour
         CharBase.OnPlayerDies += Finish;
     }
 
+    //void Start()
+    //{
+    //    for (int i = 0; i < 2; i++)
+    //    {
+    //        // キャラクターを生成
+    //        player[i] = Instantiate(characters[pick_nums[i]].chars, spawn_point[i].transform.position, Quaternion.identity);
+    //        datas[i] = player[i].GetComponent<CharBase>();
+    //        datas[i].id = i;
+
+    //        // ポインター
+    //        p_obj[i]=Instantiate(player_obj[i]);
+
+    //        // GUI
+    //        gui[i].bar.Init(player[i]);
+    //        if (player[i].TryGetComponent<CharBase>(out var p))
+    //        {
+    //            p.burst_bar = gui[i].bar;   // バーストゲージの表示
+    //            gui[i].name.text = p.data.char_name;    // 名前の表示
+    //            p.cooltimer[0] = gui[i].skill1_cooltimer;   // スキル1のクールタイム表示
+    //            p.cooltimer[1] = gui[i].skill2_cooltimer;   // スキル2のクールタイム表示
+    //            gui[i].icon.sprite = characters[pick_nums[i]].icon; // アイコン表示
+    //        }
+    //    }
+    //}
+
     void Start()
     {
+
         for (int i = 0; i < 2; i++)
         {
-            // キャラクターを生成
-            player[i] = Instantiate(characters[pick_nums[i]].chars, spawn_point[i].transform.position, Quaternion.identity);
-            datas[i] = player[i].GetComponent<CharBase>();
-            datas[i].id = i;
-
             // ポインター
             p_obj[i]=Instantiate(player_obj[i]);
 
-            // GUI
+            GameObject prefab = characters[pick_nums[i]].chars;
+
+            PlayerInput pi;
+
+            if (i == 0)
+            {
+                pi = PlayerInput.Instantiate(
+                    prefab,
+                    playerIndex: i,
+                    controlScheme: "Controller1",
+                    pairWithDevice: Gamepad.all[0]
+                );
+            }
+            else
+            {
+                pi = PlayerInput.Instantiate(
+                    prefab,
+                    playerIndex: i,
+                    controlScheme: "Controller2",
+                    pairWithDevice: Gamepad.all[1]
+                );
+            }
+
+            // ★ここが正解
+            pi.transform.position = spawn_point[i].transform.position;
+            pi.transform.rotation = Quaternion.identity;
+
+            player[i] = pi.gameObject;
+            datas[i] = player[i].GetComponent<CharBase>();
+            datas[i].id = i;
+
             gui[i].bar.Init(player[i]);
+
             if (player[i].TryGetComponent<CharBase>(out var p))
             {
-                p.burst_bar = gui[i].bar;   // バーストゲージの表示
-                gui[i].name.text = p.data.char_name;    // 名前の表示
-                p.cooltimer[0] = gui[i].skill1_cooltimer;   // スキル1のクールタイム表示
-                p.cooltimer[1] = gui[i].skill2_cooltimer;   // スキル2のクールタイム表示
-                gui[i].icon.sprite = characters[pick_nums[i]].icon; // アイコン表示
+                p.burst_bar = gui[i].bar;
+                gui[i].name.text = p.data.char_name;
+                p.cooltimer[0] = gui[i].skill1_cooltimer;
+                p.cooltimer[1] = gui[i].skill2_cooltimer;
+                gui[i].icon.sprite = characters[pick_nums[i]].icon;
             }
         }
     }
