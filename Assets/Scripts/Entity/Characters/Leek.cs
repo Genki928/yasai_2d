7756206b4bfd9cba53a3a9c4e74d1e30e6bb861a
DESKTOP-A66LBOP\GA2B <h1202515021@hiratagakuen.onmicrosoft.com>
2026-06-08 14:04:09 +0900

    no message

--- Assets/Scripts/Entity/Characters/Leek.cs
+++ Assets/Scripts/Entity/Characters/Leek.cs
@@ -1,32 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
 
-public class Leek : CharBase
+
+public class Leek : CharBase, IBurst
 {
-    //[SerializeField] Collider2D[] colliders = Collider2D[4];
+    //斬撃用
+    [SerializeField] GameObject collision;
+    [SerializeField] int skill1Damage = 20;
+
+    //カウンター用
+    [SerializeField] float counterTime = 1.0f;
+    [SerializeField] int counterDamage = 30;
+
+    private bool isCounter = false;
+
+
+
+    [SerializeField] SpriteRenderer sprite;
+    public int id { get; set; } = 0;
+
+
+    private Vector2 defaultSize;
+    private bool isHeadBanging = false;
 
     override protected void Start()
     {
         base.Start();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     override protected void Update()
     {
-        base.Update();
     }
 
     protected override void FixedUpdate()
     {
-        base.FixedUpdate();
     }
 
     public override void Skill1(InputAction.CallbackContext ctx)
     {
-        ;
+        if (!ctx.performed) return;
+
+        Vector2 spawnPos =
+            (Vector2)transform.position +
+            direction.normalized * 1.5f;
+
+        GameObject obj =
+            Instantiate(collision, spawnPos, Quaternion.identity);
+
+        HitDamageArea hit =
+            obj.GetComponent<HitDamageArea>();
+
+        hit.Init(id, skill1Damage, Vector2.zero);
     }
 
-    public override void Skill2(InputAction.CallbackContext ctx)
+    override public void Skill2(InputAction.CallbackContext ctx)
     {
-        ;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
     }
 }
+
+
