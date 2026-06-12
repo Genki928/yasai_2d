ecbd79cd6d0f0d648aa6d454936a0dccfb519377
DESKTOP-U8RCPBA\GA2B <h1202515016@hiratagakuen.onmicrosoft.com>
2026-06-12 16:30:00 +0900

    no message

--- Assets/Scripts/Entity/Boss/BossCarrot.cs
+++ Assets/Scripts/Entity/Boss/BossCarrot.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BossCarrot : BossBase
 {
     override protected void Start()
     {
         // フェーズ設定
-        state_list.Add(new BossCarrotPhase1());
+        state_list.Add(new BossCarrotTackle());
 
         // 基底クラス
         base.Start();
@@ -17,15 +18,28 @@ public class BossCarrot : BossBase
     }
 }
 
-public class BossCarrotPhase1 : BossState
+public class BossCarrotTackle : BossState
 {
+    int phase = 0;
+    int rotate = 0;
+
     override public void Enter(BossBase bb)
     {
         Debug.Log("phae 1 enter");
     }
     override public void Update(BossBase bb)
     {
-        ;
+        switch(phase)
+        {
+            case 0:
+                bb.Freeze(false);
+                bb.transform.rotation = Quaternion.Euler(0, 0, rotate += 10);
+                if (rotate >= 90) phase = 1;
+                break;
+
+            case 1:
+                break;
+        }
     }
     override public void Exit(BossBase bb)
     {
