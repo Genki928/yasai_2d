356a69897d70c8a625c97ac92a120e4c5f55e2f9
DESKTOP-A66LBOP\GA2B <h1202515021@hiratagakuen.onmicrosoft.com>
2026-06-17 12:51:21 +0900

    スイカ

---
+++ Assets/Scripts/Entity/Characters/WaterMelon.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using static UnityEditor.PlayerSettings;
+using static UnityEngine.Rendering.DebugUI;
+
+public class WaterMelon : CharBase
+{
+    [SerializeField] SpriteRenderer sprite;
+
+    public Sprite WaterMelon_default;
+    public Sprite WaterMelon_skill;
+
+
+
+    //skill1
+    [SerializeField] GameObject bullet;
+    [SerializeField]static int bullet_damage = 7;
+
+    //skill2
+    [SerializeField] GameObject cutter;
+    [SerializeField] static int cutter_damage = 30;
+
+
+
+    private Vector2 defaultSize;
+    private bool isHeadBanging = false;
+
+    override protected void Start()
+    {
+        base.Start();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    override protected void Update()
+    {
+        base.Update();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+    public override void Skill1(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            // クールタイム中なら終了
+            if (skill_1_cooltime != 0 || !can_control) return;
+
+            StartCoroutine(WaterMelonShoot());
+      
+           
+            rb.linearVelocity = Vector2.zero;
+            rigid += data.skill_1_rigid;
+            skill_1_cooltime = data.skill_1_cooltime;
+        }
+    }
+
+    private IEnumerator WaterMelonShoot()
+    {
+        sprite.sprite = WaterMelon_skill;
+        speed.generic = 2;
+        // 座標・ベクトルの算出
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        // 弾を生成 -> idの紐づけ
+        GameObject go = Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, angle));
+        go.GetComponent<DamageArea>().Init(id, bullet_damage, direction * 0.5f, true);
+        yield return new WaitForSeconds(0.1f);
+        // 座標・ベクトルの算出
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        // 弾を生成 -> idの紐づけ
+        go = Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, angle));
+        go.GetComponent<DamageArea>().Init(id, bullet_damage, direction * 0.5f, true);
+        yield return new WaitForSeconds(0.1f);
+        // 座標・ベクトルの算出
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        // 弾を生成 -> idの紐づけ
+        go = Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, angle));
+        go.GetComponent<DamageArea>().Init(id, bullet_damage, direction * 0.5f, true);
+        yield return new WaitForSeconds(0.1f);
+        sprite.sprite = WaterMelon_default;
+        speed.generic = 4;
+    }
+   
+    override public void Skill2(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            // 中断処理
+            if (skill_2_cooltime != 0 || !can_control) return;
+
+            StartCoroutine(WaterMelonCutter());
+
+            // 硬直・クールタイム
+            rb.linearVelocity = Vector2.zero;
+            rigid += data.skill_2_rigid;
+            skill_2_cooltime = data.skill_2_cooltime;
+        }
+    }
+
+    private IEnumerator WaterMelonCutter()
+    {
+        sprite.sprite = WaterMelon_skill;
+        speed.generic = 1;
+        // 座標・ベクトルの算出
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        // 弾を生成 -> idの紐づけ
+        GameObject go = Instantiate(cutter, transform.position, Quaternion.Euler(0, 0, angle - 270f));
+        go.GetComponent<DamageArea>().Init(id, cutter_damage, direction * 0.3f, true);
+        yield return new WaitForSeconds(1.0f);
+        sprite.sprite = WaterMelon_default;
+        speed.generic = 4;
+    }
+
+    public override Sprite GetDefaultImage()
+    {
+        return WaterMelon_default;
+    }
+
+    public override void Damage(int value, int id)
+    {
+        base.Damage(value, id);
+    }
+}
+
+
+
+
