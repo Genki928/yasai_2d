8026ff2d6b9f8cf20565d6c2818d1aa1605139ea
DESKTOP-A66LBOP\GA2B <h1202515021@hiratagakuen.onmicrosoft.com>
2026-06-24 11:40:20 +0900

    no message

--- Assets/Scripts/Prologue/prologue.cs
+++ Assets/Scripts/Prologue/prologue.cs
@@ -1,11 +1,15 @@
 using Const;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class prologue : MonoBehaviour
 {
+    public bool isLoad = false;
     public float speed = 50f; // 1•b‚ ‚½‚è‚ÌˆÚ“®—Ê
+
+    [SerializeField] float endY = 4300f;
 
     RectTransform rect;
 
@@ -17,10 +21,16 @@ public class prologue : MonoBehaviour
     void Update()
     {
         rect.anchoredPosition += Vector2.up * speed * Time.deltaTime;
+
+        if (rect.anchoredPosition.y >= endY)
+        {
+            Winner.sprite = null;
+            SceneManager.LoadScene(SceneName.TITLE);
+        }
     }
     public void SceneChange_Titele(InputAction.CallbackContext ctx)
     {
-                SceneManager.LoadScene(SceneName.TITLE);
-                Winner.sprite = null;
-     }
+        SceneManager.LoadScene(SceneName.TITLE);
+        Winner.sprite = null;
+    }
 }
