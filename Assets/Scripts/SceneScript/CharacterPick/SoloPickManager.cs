7491b8f3e97aeed2f06c43c9a594643a3677b5a6
DESKTOP-U8RCPBA\GA2B <h1202515016@hiratagakuen.onmicrosoft.com>
2026-07-15 16:31:05 +0900

    no message

--- Assets/Scripts/SceneScript/CharacterPick/SoloPickManager.cs
+++ Assets/Scripts/SceneScript/CharacterPick/SoloPickManager.cs
@@ -1,196 +1,80 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using Const;
 
-public class SoloPickManager : MonoBehaviour
+public class SoloPickManager : PickManagerBase
 {
-    [Header("◇アイコン")]
-    [SerializeField] GameObject icon_pf;
-    [SerializeField] List<Sprite> icon_img = new();
-    List<GameObject> icon_obj = new();
-    Vector2 pos = new(0, 5);
 
-    [Header("◇カーソル")]
-    [SerializeField] GameObject cursor_pf;
-    [SerializeField] Cursor cursor;
-    GameObject cursor_obj;
-
-    [SerializeField] List<PickData> pick_data = new();
-    [SerializeField] StateIndicater state;
-
-    [Header("◇Ready")]
-    [SerializeField] GameObject[] ready = new GameObject[2];
-
-    const float ICON_HORIZONTAL_SPACE = 1.5f;
-    const float ICON_VERTICAL_SPACE = 2.0f;
-    const int ICON_LINEFEED_COUNT = 3;
-    const int X = 0;
-    const int Y = 1;
-
-    void Start()
+    protected override void Start()
     {
-        // アイコン生成
-        for (int i = 0; i < icon_img.Count; i++)
-        {
-            icon_obj.Add(Instantiate(icon_pf));
-        }
+        base.Start();
 
-        // 移動
-        for (int i = 0; i < icon_obj.Count; i++)
-        {
-            pos = new(-ICON_HORIZONTAL_SPACE, 3);
-            // 座標決定
-            icon_obj[i].transform.position = new(pos.x + ICON_HORIZONTAL_SPACE * (i % ICON_LINEFEED_COUNT),
-                                                 pos.y + -ICON_VERTICAL_SPACE * (i / ICON_LINEFEED_COUNT));
-
-            // アイコンの変更
-            icon_obj[i].GetComponent<PickIcon>().SetIcon(icon_img[i]);
-        }
-        cursor_obj = Instantiate(cursor_pf);
-        cursor_obj.GetComponent<SpriteRenderer>().sprite = cursor.img;
-        Draw();
+        cursor_obj[0] = Instantiate(cursor_pf);
+        cursor_obj[0].GetComponent<SpriteRenderer>().sprite = cursor[0].img;
+        Draw(++current_controller);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void Interact(InputAction.CallbackContext ctx)
+    public override void Interact(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
             // すべてのプレイヤーがキャラクターを決定していたら、シーンを遷移
-            if (cursor.interact)
+            if (cursor[0].interact)
             {
-                CharPickData.id = cursor.pos[Y] * ICON_LINEFEED_COUNT + cursor.pos[X];
+                CharPickData.id = cursor[0].pos[Y] * ICON_LINEFEED_COUNT + cursor[0].pos[X];
                 if (CharPickData.id == icon_img.Count - 1) CharPickData.id = UnityEngine.Random.Range(0, icon_img.Count - 1);
                 SceneManager.LoadScene(SceneName.BATTLE_PVE);
             }
 
-            // 決定
-            cursor.interact = true;
-            state.button.SetActive(true);
-            if (cursor.interact && cursor.interact)
-            {
-                ready[0].SetActive(true);
-                ready[1].SetActive(true);
-            }
+            base.Interact(ctx);
         }
     }
 
-    public void Cancel(InputAction.CallbackContext ctx)
+    public virtual void Cancel(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
-            if (!cursor.interact)
+            if (!cursor[0].interact)
             {
                 SceneManager.LoadScene(SceneName.TITLE);
                 return;
             }
-            if (cursor.interact && cursor.interact)
-            {
-                ready[0].SetActive(false);
-                ready[1].SetActive(false);
-            }
 
-            // 決定
-            cursor.interact = false;
-            state.button.SetActive(false);
+            base.Cansel(ctx);
         }
     }
 
-    public void CursorUP(InputAction.CallbackContext ctx)
+    public override void CursorUp(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
-            // 決定済みなら移動不可
-            if (cursor.interact) return;
-
-            if (--cursor.pos[Y] < 0) cursor.pos[Y] = icon_obj.Count / ICON_LINEFEED_COUNT;
-            if (cursor.pos[Y] == icon_obj.Count / ICON_LINEFEED_COUNT)
-            {
-                if (cursor.pos[X] > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT)
-                {
-                    cursor.pos[X] = (icon_obj.Count - 1) % ICON_LINEFEED_COUNT;
-                }
-            }
-
-            Draw();
+            base.CursorUp(ctx);
         }
     }
 
-    public void CursorDown(InputAction.CallbackContext ctx)
+    public override void CursorDown(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
-            // 決定済みなら移動不可
-            if (cursor.interact) return;
-
-            if (++cursor.pos[Y] > (icon_obj.Count - 1) / ICON_LINEFEED_COUNT)
-            {
-                cursor.pos[Y] = 0;
-            }
-            if (cursor.pos[X] > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT
-                && cursor.pos[Y] == (icon_obj.Count - 1) / ICON_LINEFEED_COUNT)
-            {
-                cursor.pos[Y] = 0;
-            }
-
-            Draw();
+            base.CursorDown(ctx);
         }
     }
 
-    public void CursorLeft(InputAction.CallbackContext ctx)
+    public override void CursorLeft(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
-            // 決定済みなら移動不可
-            if (cursor.interact) return;
-
-            // 移動
-            if (--cursor.pos[X] < 0) cursor.pos[X] = ICON_LINEFEED_COUNT - 1;
-
-            // もしカーソルのX座標がアイコンのある位置から外れていたら、座標を右端に整える
-            if (cursor.pos[Y] == icon_obj.Count / ICON_LINEFEED_COUNT)
-                if (cursor.pos[X] % ICON_LINEFEED_COUNT > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT)
-                    cursor.pos[X] = (icon_obj.Count - 1) % ICON_LINEFEED_COUNT;
-
-            // 描画
-            Draw();
+            base.CursorLeft(ctx);
         }
     }
 
-    public void CursorRight(InputAction.CallbackContext ctx)
+    public override void CursorRight(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
-            // 決定済みなら移動不可
-            if (cursor.interact) return;
-
-            // 移動
-            if (++cursor.pos[X] > ICON_LINEFEED_COUNT - 1) cursor.pos[X] = 0;
-
-            // もしカーソルのX座標がアイコンのある位置から外れていたら、座標を左端に整える
-            if (cursor.pos[Y] == icon_obj.Count / ICON_LINEFEED_COUNT)
-                if (cursor.pos[X] % ICON_LINEFEED_COUNT > (icon_obj.Count - 1) % ICON_LINEFEED_COUNT)
-                    cursor.pos[X] = 0;
-
-            // 描画
-            Draw();
+            base.CursorRight(ctx);
         }
-    }
-
-    void Draw()
-    {
-        // 描画
-        cursor_obj.transform.position = new(pos.x + ICON_HORIZONTAL_SPACE * cursor.pos[X], pos.y - ICON_VERTICAL_SPACE * cursor.pos[Y]);
-        state.model.GetComponent<SpriteRenderer>().sprite = icon_img[cursor.pos[Y] * ICON_LINEFEED_COUNT + cursor.pos[X]];
-        state.name.text = pick_data[cursor.pos[Y] * ICON_LINEFEED_COUNT + cursor.pos[X]].char_name;
-        state.lore.text = pick_data[cursor.pos[Y] * ICON_LINEFEED_COUNT + cursor.pos[X]].lore;
     }
 }
 
