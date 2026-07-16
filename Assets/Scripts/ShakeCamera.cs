e0c3f40a3a13814c7bfd3178708a4a33ae0a1b68
DESKTOP-U8RCPBA\GA2B <h1202515016@hiratagakuen.onmicrosoft.com>
2026-07-16 15:52:35 +0900

    no message

--- Assets/Scripts/ShakeCamera.cs
+++ Assets/Scripts/ShakeCamera.cs
@@ -13,8 +13,8 @@ public class ShakeCamera : MonoBehaviour
         {
             if (--_shakeTime > 0)
             {
-                float w = Random.Range((int)_startPos.x + -_size, (int)_startPos.x + _size + 1) / 100.0f;
-                float h = Random.Range((int)_startPos.x + -_size, (int)_startPos.x + _size + 1) / 100.0f;
+                float w = Random.Range((int)-_size, (int)_size + 1) / 100.0f;
+                float h = Random.Range((int)-_size, (int)_size + 1) / 100.0f;
                 transform.position = new Vector3(w, h, -10);
             }
             else
