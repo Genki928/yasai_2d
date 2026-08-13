6d2fd28746410ef0b597257d5b5257d9b43d69f2
DESKTOP-SSPFE21\PC_User <i.l0vc.fr1cha@gmail.com>
2026-08-14 00:16:11 +0900

    no message

--- Assets/Scripts/_test/Damage.cs
+++ Assets/Scripts/_test/Damage.cs
@@ -8,3 +8,20 @@ public class DamageAreaBase : MonoBehaviour
     protected Vector2 _vec = new(0.0f, 0.0f);
     protected Rigidbody2D _rigidbody;
 }
+
+public enum DamageType
+{
+    Soundable,
+    Silentable
+}
+
+public class Damage
+{
+    // ----- メンバ ----- //
+    public int Value => _value;
+    public DamageType Type => _type;
+
+    // ----- プロパティ ----- //
+    int _value = 0;
+    DamageType _type = DamageType.Silentable;
+}
